6f3747ef9135a704bb4565545693e211b4f8d440
Eros <erosburastero_it@hotmail.com>
2022-03-01 23:32:03 -0300

    i

--- src/components/Data.vue
+++ src/components/Data.vue
@@ -1,25 +1,39 @@
 <template>
-  <div class="container ">
-    
-      
-    <v-card
-      max-height="500"
-      max-width="400"
-      class="my-5 ml-16"
-      elevation="2"
-      v-for="(character, index) in characters"
-      :key="index"
-    >
-      <v-img :src="character.image"></v-img>
+  <div>
+    <div class="d-flex justify-center"
+    :class="{'d-flex justify-center' :$vuetify.breakpoint.smAndDown}" >
+      <v-img
+        class="photo"
+        src="title1.png"
+        max-height="500"
+        max-width="600"
+      ></v-img>
+    </div>
 
-      <div >
-        <div class="brown darken-4 white--text title d-flex justify-center">{{ character.name }}</div>
-        
-        <div class="ml-14 ">{{ "CHARACTERISTICS:  " + character.status }} — {{ character.species }}</div>
-        
-        <div>{{ character.gender }}</div>
-      </div>
-    </v-card>
+    <div class="container">
+      <v-card
+        max-height="500"
+        max-width="400"
+        class="card my-5 ml-16"
+        :class="{ 'd-flex justify-center': $vuetify.breakpoint.smAndDown }"
+        elevation="2"
+        v-for="(character, index) in characters"
+        :key="index"
+      >
+        <v-img :src="character.image"></v-img>
+
+        <div class="black darken-4 white--text title d-flex justify-center">
+          {{ character.name }}
+        </div>
+
+        <div class="d-flex justify-center title green darken-4 white--text">
+          {{ "CHARACTERISTICS:  " + character.status }} —
+          {{ character.species }}
+        </div>
+
+        <div class="d-flex justify-center title light-blue darken-4">{{ "GENDER: " + character.gender }}</div>
+      </v-card>
+    </div>
   </div>
 </template>
 
@@ -30,7 +44,7 @@ export default {
   data() {
     return {
       characters: [],
-    }
+    };
   },
 
   methods: {
@@ -45,10 +59,7 @@ export default {
           this.characters = data.results;
         });
     },
-
-  
   },
-
 
   created() {
     this.fetchData();
@@ -57,12 +68,14 @@ export default {
 </script>
 
 <style>
-
-
 .container {
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
   grid-template-rows: 1fr;
   gap: 10px 10px;
+}
+
+.card {
+  border: solid grey 2px;
 }
 </style>
